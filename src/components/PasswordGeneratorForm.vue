de1e9a375a0f3fb865ea27335ee168170ab84294
MarkAlexI <markovaleksandr108@gmail.com>
2024-01-14 15:21:06 +0200

    Added simple generator.

--- src/components/PasswordGeneratorForm.vue
+++ src/components/PasswordGeneratorForm.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>
-    There will be form.
+  <div class="container">
+    <div>
+      <label for="passwordLength">Password length:</label>
+      <input type="number" v-model="passwordLength" min="6" max="20"</input>
+    </div>
+    <div>
+      <label for="includeUppercase">Include uppercase:</label>
+      <input type="checkbox" v-model="includeUppercase" </input>
+    </div>
+    <div>
+      <label for="includeNumbers">Include numbers:</label>
+      <input type="checkbox" v-model="includeNumbers" </input>
+    </div>
+    <button class="btn" @click="generatePassword">Generate Password</button>
+    <p>{{ generatedPassword }}</p>
   </div>
 </template>
-
 <script setup>
-  console.log('form');
+  import { ref } from 'vue';
+  const passwordLength = ref(8);
+  const includeUppercase = ref(true);
+  const includeNumbers = ref(true);
+  const generatedPassword = ref('');
+  const generatePassword = () => generatedPassword.value = 'passw0rd';
 </script>
