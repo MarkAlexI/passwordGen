5058c42ea247da033b911b16e57c806b60fe91e7
MarkAlexI <markovaleksandr108@gmail.com>
2024-01-27 15:04:53 +0200

    Added special chars to generated password.

--- src/composition/usePasswordGenerator.js
+++ src/composition/usePasswordGenerator.js
@@ -4,16 +4,19 @@ export default function usePasswordGenerator() {
   const passwordLength = ref(12);
   const includeUppercase = ref(true);
   const includeNumbers = ref(true);
+  const includeSpecialChars = ref(false);
   const generatedPassword = ref('');
 
   const generatePassword = () => {
     const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
     const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const numberChars = '0123456789';
+    const specialChars = '#$_&%(){}[]';
 
     let chars = lowercaseChars;
     if (includeUppercase.value) chars += uppercaseChars;
     if (includeNumbers.value) chars += numberChars;
+    if (includeSpecialChars.value) chars += specialChars;
 
     let password = '';
     for (let i = 0; i < passwordLength.value; i++) {
@@ -28,6 +31,7 @@ export default function usePasswordGenerator() {
     passwordLength,
     includeUppercase,
     includeNumbers,
+    includeSpecialChars,
     generatedPassword,
     generatePassword,
   };
