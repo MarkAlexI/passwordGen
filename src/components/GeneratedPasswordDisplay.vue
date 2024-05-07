a5d245145e0c890c3ebccd8cc45fdb4427509078
MarkAlexI <markovaleksandr108@gmail.com>
2024-05-07 18:16:11 +0300

    Added copying to clipboard.

--- src/components/GeneratedPasswordDisplay.vue
+++ src/components/GeneratedPasswordDisplay.vue
@@ -2,17 +2,70 @@
   <div>
     <strong>Generated Password:</strong>
     <p>{{ generatedPassword }}</p>
+    <button
+      class="copy-btn"
+      @click="copyPassword"
+    >Copy!</button>
   </div>
 </template>
 
 <script setup>
-  defineProps(["generatedPassword"]);
+
+  const props = defineProps(["generatedPassword"]);
+
+  let copyPassword = () => {
+    console.log("#");
+  };
+
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state == "granted" || result.state == "prompt") {
+      alert("Write access granted!");
+
+      let text = props.generatedPassword;
+
+      copyPassword = async () => {
+        try {
+          await navigator.clipboard.writeText(text);
+          console.log("Content copied to clipboard");
+        } catch (error) {
+          console.error("Failed to copy: ", err);
+        }
+      };
+    }
+  });
 </script>
 
-<style scoped lang="scss">
-  p {
-    text-align: center;
-    color: blue;
-    word-break: break-all;
+<style lang="scss">
+$light-text-color: white;
+$dark-bg: #222;
+$dark-btn-border: #555;
+$btn-border-radius: 1.5rem;
+$btn-padding: .5em 1.5em;
+$btn-margin: 1rem auto;
+
+p {
+  text-align: center;
+  color: $light-text-color;
+  word-break: break-all;
+}
+
+.copy-btn {
+  background: $dark-bg;
+  border: solid 1px $dark-btn-border;
+  border-radius: $btn-border-radius;
+  font: inherit;
+  padding: $btn-padding;
+  margin-bottom: $btn-margin;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+
+  &:hover {
+    background: darken($dark-bg, 10%);
   }
+
+  &:active {
+    background: lighten($dark-bg, 10%);
+  }
+}
 </style>
