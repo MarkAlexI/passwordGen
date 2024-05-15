a97930bfa49ee24f1056e7b0b7329f1a717434bc
MarkAlexI <markovaleksandr108@gmail.com>
2024-05-15 20:26:22 +0300

    Updated test for GeneratedPasswordDisplay.

--- src/components/__tests__/GeneratedPasswordDisplay.spec.js
+++ src/components/__tests__/GeneratedPasswordDisplay.spec.js
@@ -3,10 +3,25 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import GeneratedPasswordDisplay from '../GeneratedPasswordDisplay.vue'
 
-describe('GeneratedPasswordDisplay', () => {
+/*describe('GeneratedPasswordDisplay', () => {
   it('renders properly', () => {
     const wrapper = mount(GeneratedPasswordDisplay)
     expect(wrapper.text()).toContain('Generated Password:Copy!')
+  })
+})*/
+describe('GeneratedPasswordDisplay', () => {
+  it('renders properly', () => {
+    const generatedPassword = 'TestPassword123'
+    const wrapper = mount(GeneratedPasswordDisplay, {
+      props: {
+        generatedPassword: generatedPassword
+      }
+    })
+
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.text()).toContain('Generated Password:')
+    expect(wrapper.text()).toContain(generatedPassword)
+    expect(wrapper.find('.copy-btn').exists()).toBe(true)
   })
 })
 
